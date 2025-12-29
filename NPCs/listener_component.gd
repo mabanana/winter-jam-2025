6cfb17284bedfed9b80d5ac94e1ff2b8552a2e21
mabanana <aldwin.yu@hotmail.com>
2025-12-29 17:03:21 -0500

    Add arrow for detection animation

--- NPCs/listener_component.gd
+++ NPCs/listener_component.gd
@@ -1,4 +1,12 @@
+@tool
 extends Area2D
 class_name ListenerComponent
 
 @export var character: Node2D
+@export var arrow: Node2D
+@export var arrow_offset: Vector2 = Vector2(0, -8)
+
+func _ready():
+	if !Engine.is_editor_hint():
+		arrow.hide()
+	arrow.position = arrow_offset
